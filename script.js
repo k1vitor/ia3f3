92f5bb95fed0ebae1b967c99f706f88572d7f7b9
k1vitor <161651632+k1vitor@users.noreply.github.com>
2024-08-01 16:47:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const caixaprincipal = document.querySelector('.caixa-principal');
+const caixaperguntas = document.querySelector('.caixa-pergunta');
+const caixalternativa = document.querySelector('.caixa-alternativa');
+const caixaresultados = document.querySelector('.caixa-resultados');
+
+const perguntas = [
+
+anunciado: "pergunta 1"
+alternativas: [
+              "alternativa 1",
+              "alternativa 2"
+
+]
+]
