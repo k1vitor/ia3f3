bcbe64300af0dc66e0c2f533ca0fc782430ce0f9
k1vitor <161651632+k1vitor@users.noreply.github.com>
2024-09-25 18:25:25 -0300

    Add files via upload

---
+++ IA3F-main/script.js
@@ -0,0 +1,42 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
+
+const perguntas = [ //serve para abrir lista de perguntas
+    {//abre o objeto das perguntas
+        enunciado: "Pergunta 1",
+    alternativas: [{texto:"Alternativa 1",afirmação:"afirmação da alternativa 1"}
+        
+        ,{texto:"Alternativa 2",afirmação:"afirmação da alternativa 2"}]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [{texto:"Alternativa 1",afirmação:"afirmação da alternativa 1"}
+            
+        , {texto:"Alternativa 2",afirmação:"afirmação da alternativa 2"}]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: [{texto:"Alternativa 1",afirmação:"afirmação da alternativa 1"}
+            
+            ,{texto:"Alternativa 2",afirmação:"afirmação da alternativa 2"}]
+    }
+]
+
+let posicao = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[posicao];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+mostraPergunta();
